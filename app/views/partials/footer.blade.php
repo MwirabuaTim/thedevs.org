d8dbc5eb83e1282564f46aec1e2e445ecaa0efd6
techytimo <techytimo@blackpearl.localdomain>
2014-02-25 12:08:18 +0300

    temporarily disabled contactus link at footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
             <li class="link"><a class="_demo" href="{{ URL::to('howitworks') }}">&#8226;&nbsp;How it Works&nbsp;</a></li>
             <li class="link"><a href="{{ URL::to('tos') }}">&#8226;&nbsp;Terms of Use&nbsp;</a></li>
             <li class="link"><a href="{{ URL::to('privacy') }}">&#8226;&nbsp;Privacy Policy&nbsp;</a></li>
-            <li class="link"><a href="{{ URL::to('contactus') }}">&#8226;&nbsp;Contact Us&nbsp;</a></li>
+            <!-- <li class="link"><a href="{{ URL::to('contactus') }}">&#8226;&nbsp;Contact Us&nbsp;</a></li> -->
         </ul>
 
         <div class="_follow _in-block">
