b393b7fc0f63cdad8e27bd14d9607b453b57433f
techytimo <techytimo@blackpearl.localdomain>
2014-03-20 20:57:39 +0300

    testing oauthcontroller

--- app/controllers/OauthController.php
+++ app/controllers/OauthController.php
@@ -65,7 +65,7 @@ class OauthController extends BaseController {
 	            // If you store it all in a cookie and redirect to a registration page this is crazy-simple.
 
 	            // return Response::json($data);
-	            // return Profile::where('uid', $data['uid'])->first()
+	            return Profile::where('uid', $data['uid'])->first();
 	            return User::where('email', $data['email'])->first();
 	
 	            $user_id = $this->checkAndSave($data);
