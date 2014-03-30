8df1c80c394a258f7b201fa64b75ceae84a07587
techytimo <techytimo@blackpearl.localdomain>
2014-03-30 18:55:30 +0300

    edited forgot-password-confirm.success message

--- app/lang/en/auth/message.php
+++ app/lang/en/auth/message.php
@@ -5,7 +5,7 @@ return array(
 	'signin.success' => 'Successfully Signed In.',
 	'account_not_found' => 'There is no account by '. Input::get('email'),
 	'account_not_activated' => 'Check email sent to '. Input::get('email').' to activate the account.',
-	'account_suspended' => 'Your account is suspended, come back later.',
+	'account_suspended' => 'Your account is suspended. Please come back later.',
 	'account_banned' => 'Sorry, your account is banned.',
 	'signup.success' => 'Successfull. Check '. Input::get('email'). ' for a link to activate your account.',
 	'account_already_exists' => Input::get('email').' is already registered. An activation link was emailed.',
@@ -14,7 +14,7 @@ return array(
 	// 'activate.error' => 'Activation Error',
 	'forgot-password.success' => 'Successfull. Check '. Input::get('email'). ' for a link to reset your password.',
 	// 'account_not_found' => 'There is no account by that email.',
-	'forgot-password-confirm.success' => 'Confirmation Success',
+	'forgot-password-confirm.success' => 'Successfull. You can now log in.',
 	'forgot-password-confirm.error' => 'Ooops, we have an error',
 	// 'auth/message.account_not_found' => 'auth/message.account_not_found',
 	'wrong_password' => 'You provided the wrong password!', //by tim
