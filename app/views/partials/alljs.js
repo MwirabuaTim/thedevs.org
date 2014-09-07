5edb4d0f2ebd0c8c1da9199baece017687d4bfc3
techytimo <techytimo@blackpearl.localdomain>
2014-09-07 19:48:39 +0300

    osm map tiles

--- app/views/partials/alljs.js
+++ app/views/partials/alljs.js
@@ -161,7 +161,8 @@
     L.tileLayer(
     // 'http://{s}.tile.cloudmade.com/5e9427487a6142f7934b07d07a967ba3/997/256/{z}/{x}/{y}.png', {
     // 'http://{s}.tile.cloudmade.com/5E9427487A6142F7934B07D07A967BA3/997/256/{z}/{x}/{y}.png', {
-    'http://a.tiles.mapbox.com/v3/techytimo.ic27c2cm/{z}/{x}/{y}.png', {
+    // 'http://a.tiles.mapbox.com/v3/techytimo.ic27c2cm/{z}/{x}/{y}.png', {
+      'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         // attribution: '',
         maxZoom: 18,
         // noWrap: true,
